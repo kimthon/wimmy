d277e90041ed7695baec821752ac0d985b9c0117
jhlee95 <ljh951103@naver.com>
2020-02-11 00:22:49 +0900

    manifest, icon

--- app/src/main/java/com/example/wimmy/MainActivity.kt
+++ app/src/main/java/com/example/wimmy/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.example.wimmy
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val go_intent = findViewById(R.id.testbtn) as Button
+        go_intent.setOnClickListener {
+            val intent = Intent(this@MainActivity, MapActivity::class.java)
+            startActivity(intent)
+        }
     }
+
+
+
 }
