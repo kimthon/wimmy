ac073fb14dc5c3000fcfca6d52dbaf49f0103c0c
kimthon <xoghks9793@gmail.com>
2020-02-12 15:53:44 +0900

    set basic toobar

--- app/src/main/java/com/example/wimmy/MainActivity.kt
+++ app/src/main/java/com/example/wimmy/MainActivity.kt
@@ -1,14 +1,14 @@
 package com.example.wimmy
 
-import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
+import androidx.appcompat.widget.Toolbar
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import kotlinx.android.synthetic.main.activity_main.*
-
 
 class MainActivity : AppCompatActivity() {
     var photoList = arrayListOf<PhotoData>(
@@ -58,14 +58,34 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        SetView()
+        SetHeader()
+    }
+
+    fun SetView() {
         val recyclerView = findViewById<RecyclerView>(R.id.mRecycleView)
         val mainAdapter = MainAdapter(this, photoList)
         recyclerView.adapter = mainAdapter
         val lm = GridLayoutManager(this, 3)
         recyclerView.layoutManager = lm
-
     }
 
+    fun SetHeader() {
+        val toolbar = findViewById<Toolbar>(R.id.main_toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setTitle(null)
+    }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item?.itemId) {
+            //R.id.favorate_menu =>
+            //
+        }
+        return super .onOptionsItemSelected(item)
+    }
 }
