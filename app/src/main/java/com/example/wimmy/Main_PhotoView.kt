58037717c77b73e1978de2eceb1cfaa73d19eab3
JHLee <ljh951103@naver.com>
2020-03-10 12:55:56 +0900

    API 초기 설정 및 테스트, 로딩중 화면, 리스트 변경, 스플래쉬 액티비티 변경

--- app/src/main/java/com/example/wimmy/Main_PhotoView.kt
+++ app/src/main/java/com/example/wimmy/Main_PhotoView.kt
@@ -1,8 +1,10 @@
 package com.example.wimmy
 
 import android.app.Activity
+import android.app.ProgressDialog
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import android.os.SystemClock
 import android.util.DisplayMetrics
 import android.util.Log
@@ -22,62 +24,41 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.wimmy.Adapter.RecyclerAdapterPhoto
 import com.example.wimmy.db.*
 import kotlinx.android.synthetic.main.main_photoview.*
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.collections.ArrayList
 import java.io.File
 import java.lang.Thread.sleep
+import kotlin.coroutines.CoroutineContext
 
 
-class Main_PhotoView: AppCompatActivity() {
-    private var tagList = ArrayList<TagData>()
-    private var photoList = arrayListOf<PhotoData>()
+class Main_PhotoView: AppCompatActivity(), CoroutineScope {
+    companion object {
+        var tagList = ArrayList<TagData>()
+        var photoList = arrayListOf<PhotoData>()
+    }
     private var recyclerAdapter : RecyclerAdapterPhoto?= null
     private var recyclerView: RecyclerView? = null
     private var mLastClickTime: Long = 0
     private var delete_check: Int = 0
+    private var progressDialog: ProgressDialog? = null
 
+    private lateinit var mJob: Job
+    override val coroutineContext: CoroutineContext
+        get() = mJob + Dispatchers.Main
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_photoview)
         val view: View = findViewById(R.id.photo_recyclerView)
-
+        mJob = Job()
         getExtra(view)
         SetHeader()
-        setView(view)
-        updown_Listener(recyclerView)
-
-
-        val onScrollListener = object:RecyclerView.OnScrollListener() {
-            var temp: Int = 0
-            override fun onScrolled(@NonNull recyclerView:RecyclerView, dx:Int, dy:Int) {
-                if(temp == 1) {
-                    super.onScrolled(recyclerView, dx, dy)
-                    up_button.visibility = View.GONE
-                    down_button.visibility = View.GONE
-                }
-            }
-            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
-                super.onScrollStateChanged(recyclerView, newState)
-                up_button.visibility = View.VISIBLE
-                down_button.visibility = View.VISIBLE
-                temp = 1
-            }
-
-        }
-        recyclerView?.setOnScrollListener(onScrollListener)
 
     }
 
-    override fun onResume() {
-        super.onResume()
-        setPhotoSize(3, 3)
-    }
+
 
     private fun setView(view : View) {
         recyclerView = view.findViewById<RecyclerView>(R.id.photo_recyclerView)
@@ -89,11 +70,6 @@ class Main_PhotoView: AppCompatActivity() {
 
                 val intent = Intent(this, PhotoViewPager::class.java)
                 intent.putExtra("photo_num", num)
-                intent.putExtra("thumbnail", PhotoData.photo_id)
-
-                intent.putParcelableArrayListExtra("photo_list", photoList)
-                intent.putParcelableArrayListExtra("tag_list", tagList)
-                
                 startActivityForResult(intent, 100)
 
                 //}
@@ -128,59 +104,69 @@ class Main_PhotoView: AppCompatActivity() {
         if (resultCode == Activity.RESULT_OK) {
             when (requestCode) {
                 100 -> {
-                    if(data!!.hasExtra("delete_list")) {
-                        PhotoList = data!!.getSerializableExtra("delete_list") as ArrayList<PhotoData>
+                    if(data!!.hasExtra("delete_check")) {
                         setView(photo_recyclerView)
                         setPhotoSize(3, 3)
                         delete_check = 1
                     }
                     val doc = data!!.getIntExtra("index", 0)
-                    recyclerView?.smoothScrollToPosition(doc)
+                    recyclerView?.scrollToPosition(doc)
                 }
             }
         }
     }
 
     fun getExtra(view: View){
-        val getname: String?
         val title_type: ImageView = findViewById(R.id.title_type)
         val title: TextView = findViewById(R.id.title_name)
-        if (intent.hasExtra("dir_name")) {
-            getname = intent.getStringExtra("dir_name")
 
-            photoList = MediaStore_Dao.getNameDir(view.context, getname)
+        GlobalScope.launch(Dispatchers.Main) {
+            loading()
+            if (intent.hasExtra("dir_name")) {
+                val getname = intent.getStringExtra("dir_name")
+                title_type.setImageResource(R.drawable.ic_folder)
+                title.text = File(getname).name
 
-            title_type.setImageResource(R.drawable.ic_folder)
-            title.text = File(getname).name
+                mJob = CoroutineScope(Dispatchers.IO).launch {
+                    photoList = MediaStore_Dao.getNameDir(view.context, getname)
+                }
+
+            } else if (intent.hasExtra("location_name")) {
+                val getname = intent.getStringExtra("location_name")
+                title_type.setImageResource(R.drawable.ic_location)
+                title.text = getname
+
+                photoList = MediaStore_Dao.getLocationDir(view.context, getname)
+
+            } else if (intent.hasExtra("date_name")) {
+                val date = intent.getLongExtra("date_name", 0)
+                val cal = Calendar.getInstance()
+
+                cal.time = Date(date)
+                val formatter = SimpleDateFormat("yyyy년 MM월 dd일")
+                val getname = formatter.format(Date(date))
+                title_type.setImageResource(R.drawable.ic_cal)
+                title.text = getname
+
+                photoList = MediaStore_Dao.getDateDir(view.context, cal)
+
+            } else if (intent.hasExtra("tag_name")) {
+                /*
+                getname = intent.getStringExtra("tag_name")
+                val vm = ViewModelProviders.of(this).get(PhotoViewModel::class.java)
+                title_type.setImageResource(R.drawable.ic_tag)
+                title.text = getname
+
+                photoList = MediaStore_Dao.getTagDir(view.context, vm, getname)*/
+
+            }
+            mJob.join()
+            loadingEnd()
+            setView(view)
+            setPhotoSize(3, 3)
+            updownEvent()
         }
-        else if (intent.hasExtra("location_name")) {
-            getname = intent.getStringExtra("location_name")
-            photoList = MediaStore_Dao.getLocationDir(view.context, getname)
 
-            title_type.setImageResource(R.drawable.ic_location)
-            title.text = getname
-        }
-        else if(intent.hasExtra("date_name")) {
-            val date = intent.getLongExtra("date_name", 0)
-            val cal = Calendar.getInstance()
-
-            cal.time = Date(date)
-            photoList = MediaStore_Dao.getDateDir(view.context, cal)
-            val formatter = SimpleDateFormat("yyyy년 MM월 dd일")
-            getname = formatter.format(Date(date))
-
-            title_type.setImageResource(R.drawable.ic_cal)
-
-            title.text = getname
-        }
-        else if (intent.hasExtra("tag_name")) {
-            getname = intent.getStringExtra("tag_name")
-            val vm = ViewModelProviders.of(this).get(PhotoViewModel::class.java)
-            photoList = MediaStore_Dao.getTagDir(view.context, vm, getname)
-
-            title_type.setImageResource(R.drawable.ic_tag)
-            title.text = getname
-        }
     }
 
     fun updown_Listener(view: RecyclerView?) {
@@ -197,12 +183,57 @@ class Main_PhotoView: AppCompatActivity() {
         finishActivity()
     }
 
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mJob.cancel()
+    }
     private fun finishActivity() {
         val intent = Intent()
         if(delete_check == 1)
             intent.putExtra("delete_check", delete_check)
         setResult(Activity.RESULT_OK, intent)
         finish()
+    }
+
+    fun loading() {
+        //로딩
+        Handler().postDelayed(
+            {
+                progressDialog = ProgressDialog(this@Main_PhotoView, android.R.style.Theme_DeviceDefault_Light_Dialog_NoActionBar_MinWidth)
+                progressDialog!!.setIndeterminate(true)
+                progressDialog!!.setMessage("이미지를 불러오고 있습니다..\n잠시만 기다려 주세요")
+                progressDialog!!.show()
+            }, 0
+        )
+    }
+
+    fun loadingEnd() {
+        Handler().postDelayed(
+            { progressDialog?.dismiss() }, 0
+        )
+    }
+
+    fun updownEvent() {
+        updown_Listener(recyclerView)
+        val onScrollListener = object : RecyclerView.OnScrollListener() {
+            var temp: Int = 0
+            override fun onScrolled(@NonNull recyclerView: RecyclerView, dx: Int, dy: Int) {
+                if (temp == 1) {
+                    super.onScrolled(recyclerView, dx, dy)
+                    up_button.visibility = View.GONE
+                    down_button.visibility = View.GONE
+                }
+            }
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+                up_button.visibility = View.VISIBLE
+                down_button.visibility = View.VISIBLE
+                temp = 1
+            }
+        }
+
+        recyclerView?.setOnScrollListener(onScrollListener)
     }
 
 }
--- app/src/main/java/com/example/wimmy/db/PhotoData.kt
+++ app/src/main/java/com/example/wimmy/db/PhotoData.kt
@@ -21,41 +21,7 @@ class PhotoData(var photo_id: Long,
                 var file_path : String,
                 var location_info : String?,
                 var date_info : Date?,
-                var favorite : Boolean): Parcelable {
-    constructor(parcel: Parcel) : this(
-        parcel.readLong(),
-        parcel.readString().toString(),
-        parcel.readString().toString(),
-        parcel.readString().toString(),
-        parcel.readDate(),
-        parcel.readByte() != 0.toByte()
-    )
-
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeLong(photo_id)
-        parcel.writeString(name)
-        parcel.writeString(file_path)
-        parcel.writeString(location_info)
-        parcel.writeDate(date_info)
-        parcel.writeByte(if (favorite) 1 else 0)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<PhotoData> {
-        override fun createFromParcel(parcel: Parcel): PhotoData {
-            return PhotoData(parcel)
-        }
-
-        override fun newArray(size: Int): Array<PhotoData?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
-
+                var favorite : Boolean)
 
 @Entity(tableName = "tag_data",
     primaryKeys = ["photo_id", "tag"]
@@ -63,34 +29,7 @@ class PhotoData(var photo_id: Long,
 class TagData(
     var photo_id: Long,
     var tag: String,
-    @ColumnInfo var type: String): Parcelable {
-    constructor(parcel: Parcel) : this(
-        parcel.readLong(),
-        parcel.readString().toString(),
-        parcel.readString().toString()
-    ) {
-    }
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeLong(photo_id)
-        parcel.writeString(tag)
-        parcel.writeString(type)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<TagData> {
-        override fun createFromParcel(parcel: Parcel): TagData {
-            return TagData(parcel)
-        }
-
-        override fun newArray(size: Int): Array<TagData?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+    @ColumnInfo var type: String)
 
 
 data class thumbnailData( var photo_id : Long,
