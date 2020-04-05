4e38e3752e2caf7c94c885512dd378eaf352f1c5
JHLee <ljh951103@naver.com>
2020-04-06 01:36:58 +0900

    리스트 옵션 사진 안나오는 버그 픽스 백스택 버그 픽스 패딩 값 조정 메인포토뷰 스크롤 깜빡임 버그 픽스 마커 타이틀 활성화 기타 버그픽스

--- app/src/main/java/com/example/wimmy/Activity/Main_PhotoView.kt
+++ app/src/main/java/com/example/wimmy/Activity/Main_PhotoView.kt
@@ -6,7 +6,6 @@ import android.database.Cursor
 import android.os.Bundle
 import android.os.SystemClock
 import android.util.DisplayMetrics
-import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
