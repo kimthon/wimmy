b216feb96015dcd2f9123108fd268c3e3895be14
ljh951103 <ljh951103@naver.com>
2020-05-16 02:51:26 +0900

    Img Count, scheduleDialog

--- app/src/main/java/com/jtsoft/wimmy/fragment/DateFragment.kt
+++ app/src/main/java/com/jtsoft/wimmy/fragment/DateFragment.kt
@@ -6,7 +6,6 @@ import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.util.DisplayMetrics
-import android.util.Log
 import android.view.GestureDetector
 import android.view.LayoutInflater
 import android.view.View
